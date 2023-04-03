55854ae2d370d832263a1ec31c063237ca25352f
Prajeevan <prajee.bajee@gmail.com>
2023-04-03 11:58:56 +0530

    feat: write functions to show the questions on GUI

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import math
 import tkinter as tk
 import operator
 
+
+class StdoutRedirector:
+    def __init__(self, widget):
+        self.widget = widget
+
+    def write(self, text):
+        self.widget.insert('end', text)
+        self.widget.see('end')
+    
 # Math function
 def number_generator():
     num_1 = random.randrange(1, 20)
     num_2 = random.randrange(num_1,20)
     return num_1, num_2
-num_1, num_2 = number_generator()
 
 def question_maker(num_1, num_2):
     operators = {
@@ -25,10 +33,8 @@ def question_maker(num_1, num_2):
     if operations == '/':
         print("Answer is " ,"%.2f" % calculator)
     else:   
-        # print(div_que)
         print("Answer is " ,calculator)
-
-# question_maker(num_1,num_2)
+    return div_que, calculator
 
 #Main window 
 main_window = tk.Tk() 
@@ -41,8 +47,25 @@ title_label = tk.Label(main_window, text="Puzzle Solver v0.1.0", font=("Comic Sa
 title_label.pack(side="top", fill="x")
 
 # Question label 
-question_label = tk.Label(main_window, text="Solve this puzzle", font=("Arial", 18))
-question_label.place(x=10, y=60)
+question_label = tk.Label(main_window, text="Answer this puzzle", font=("Arial", 18))
+question_label.place(x=10, y=70)
 
+# Main label and answer label
+main_label = tk.Label(main_window, text="", font=("Arial", 18))
+main_label.place(x=10, y=100)
+answer_label = tk.Label(main_window, text="", font=("Arial", 18))
+answer_label.place(x=10, y=130)
+
+def update_problem():
+    num_1, num_2 = number_generator()
+    div_que, answer = question_maker(num_1, num_2)
+    main_label.config(text=div_que)
+    answer_label.config(text=f"Answer is {answer}")
+    
+update_problem()
+
+# Button to generate new problem
+new_problem_button = tk.Button(main_window, text="New Problem", command=update_problem)
+new_problem_button.place(x=10, y=170)
 
 main_window.mainloop()
